5d0e5c4109d343b0ec3111ba67eb775877fe2904
ac <andrzej.cabala@sikt.no>
2024-08-01 09:59:02 +0200

    FSP-477 Remove unnecessary files from tests using @notGenerated.

--- src/test/resources/query/multipleAliasesForSameType/expectedOutput/QueryDBQueries.java
+++ src/test/resources/query/multipleAliasesForSameType/expectedOutput/QueryDBQueries.java
@@ -1,15 +1,17 @@
 package fake.code.generated.queries.query;
+
 import static no.sikt.graphitron.jooq.generated.testdata.Keys.*;
 import static no.sikt.graphitron.jooq.generated.testdata.Tables.*;
+
 import fake.graphql.example.model.Film;
 import fake.graphql.example.model.Language;
-import java.lang.String;
 import no.fellesstudentsystem.graphql.helpers.selection.SelectionSet;
 import org.jooq.DSLContext;
 import org.jooq.Functions;
 import org.jooq.impl.DSL;
+
 public class QueryDBQueries {
-    public static Film filmForQuery(DSLContext ctx, String id, SelectionSet select) {
+    public static Film filmForQuery(DSLContext ctx, SelectionSet select) {
         var film_filmlanguageidfkey_left = FILM.filmLanguageIdFkey().as("filmLanguageIdFkey_2782157680");
         var film_filmoriginallanguageidfkey_left = FILM.filmOriginalLanguageIdFkey().as("filmOriginalLanguageIdFkey_1523086221");
         return ctx
@@ -23,7 +25,6 @@ public class QueryDBQueries {
                 .from(FILM)
                 .leftJoin(film_filmlanguageidfkey_left)
                 .leftJoin(film_filmoriginallanguageidfkey_left)
-                .where(FILM.ID.eq(id))
                 .fetchOne(it -> it.into(Film.class));
     }
 }
--- src/test/resources/query/queryWithoutPagination/expectedOutput/QueryDBQueries.java
+++ src/test/resources/query/queryWithoutPagination/expectedOutput/QueryDBQueries.java
@@ -1,25 +1,19 @@
 package fake.code.generated.queries.query;
+
 import static no.sikt.graphitron.jooq.generated.testdata.Keys.*;
 import static no.sikt.graphitron.jooq.generated.testdata.Tables.*;
+
 import fake.graphql.example.model.Film;
-import java.lang.String;
-import java.util.List;
 import no.fellesstudentsystem.graphql.helpers.selection.SelectionSet;
 import org.jooq.DSLContext;
 import org.jooq.Functions;
 import org.jooq.impl.DSL;
+
 public class QueryDBQueries {
-    public static List<Film> filmsForQuery(DSLContext ctx, String releaseYear, SelectionSet select) {
+    public static Film filmsForQuery(DSLContext ctx, SelectionSet select) {
         return ctx
-                .select(
-                        DSL.row(
-                                FILM.getId(),
-                                select.optional("title", FILM.TITLE)
-                        ).mapping(Functions.nullOnAllNull(Film::new))
-                )
+                .select(DSL.row(FILM.getId()).mapping(Functions.nullOnAllNull(Film::new)))
                 .from(FILM)
-                .where(FILM.RELEASE_YEAR.eq(releaseYear))
-                .orderBy(FILM.getIdFields())
-                .fetch(it -> it.into(Film.class));
+                .fetchOne(it -> it.into(Film.class));
     }
 }
