a37a213f69a042cd49594484a6c6a4ea9c1c8580
Andrea Nakstad <andrea.nakstad@sikt.no>
2025-05-23 14:45:42 +0200

    Add graphitron-example queries + tests

--- graphitron-example/graphitron-example-service/src/main/java/no/sikt/graphitron/example/service/CustomerService.java
+++ graphitron-example/graphitron-example-service/src/main/java/no/sikt/graphitron/example/service/CustomerService.java
@@ -1,9 +1,8 @@
 package no.sikt.graphitron.example.service;
 
 import no.sikt.graphitron.example.generated.jooq.tables.records.CustomerRecord;
+import no.sikt.graphitron.example.service.records.HelloWorldInput;
 import org.jooq.DSLContext;
-
-import static no.sikt.graphitron.example.generated.jooq.Tables.CUSTOMER;
 
 public class CustomerService {
     public CustomerService(DSLContext context) {
@@ -11,5 +10,18 @@ public class CustomerService {
 
     public CustomerRecord customer() {
         return null;
+    }
+
+    public CustomerRecord customer(CustomerRecord input) {
+        var customer = new CustomerRecord();
+        customer.setCustomerId(input.getCustomerId());
+        return customer;
+    }
+
+    public CustomerRecord customer(HelloWorldInput input) {
+        // We don't have access to nodeIdStrategy here to decode and use customerId
+        var customer = new CustomerRecord();
+        customer.setFirstName(input.getName());
+        return customer;
     }
 }
