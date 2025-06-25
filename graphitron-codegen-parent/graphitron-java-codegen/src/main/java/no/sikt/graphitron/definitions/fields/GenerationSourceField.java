fd290454dab4c873d8d9e50e5476926ed10b2eed
Kenneth Pettersen Lund <2337640+kennethpl@users.noreply.github.com>
2025-06-26 01:21:23 +0200

    GGG-45: Perform minor code cleanup

--- graphitron-codegen-parent/graphitron-java-codegen/src/main/java/no/sikt/graphitron/definitions/fields/GenerationSourceField.java
+++ graphitron-codegen-parent/graphitron-java-codegen/src/main/java/no/sikt/graphitron/definitions/fields/GenerationSourceField.java
@@ -220,6 +220,10 @@ public abstract class GenerationSourceField<T extends NamedNode<T> & DirectivesC
         return hasCondition() && condition.isOverride();
     }
 
+
+    /**
+     * A field is considered generated unless the `@notGenerated` directive is applied.
+     */
     @Override
     public boolean isGenerated() {
         return isGenerated;
@@ -248,6 +252,11 @@ public abstract class GenerationSourceField<T extends NamedNode<T> & DirectivesC
         return null;
     }
 
+
+    /**
+     * A field is considered generated with a resolver if it is generated and satisfies one of the following
+     * conditions: it is a resolver, it is a field in the root type `Query`, or it is a field in the `Mutation` type.
+     */
     @Override
     public boolean isGeneratedWithResolver() {
         return isGeneratedAsResolver;
