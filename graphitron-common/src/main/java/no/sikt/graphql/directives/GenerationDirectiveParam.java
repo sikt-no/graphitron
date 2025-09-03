7815e5d5e5b3148bb7927ce83e6f750ba7887096
Andrea N <57245846+andreahn@users.noreply.github.com>
2025-09-03 14:12:45 +0200

    Breaking: Change 'references' argument in @reference to 'path' (#116)

--- graphitron-common/src/main/java/no/sikt/graphql/directives/GenerationDirectiveParam.java
+++ graphitron-common/src/main/java/no/sikt/graphql/directives/GenerationDirectiveParam.java
@@ -11,7 +11,7 @@ public enum GenerationDirectiveParam {
     TABLE("table"),
     CONDITION("condition"),
     OVERRIDE("override"),
-    REFERENCES("references"),
+    PATH("path"),
     TYPE("typeName"),
     ENUM("enumReference"),
     SERVICE("service"),
