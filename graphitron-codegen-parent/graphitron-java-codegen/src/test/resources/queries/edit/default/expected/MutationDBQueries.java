062de5ca93a996e80ca6871f1f21649d4dff78c5
Christoffer <christoffer.hansen@sikt.no>
2025-10-09 12:15:34 +0200

    Fix upsert with node id strategy

--- graphitron-codegen-parent/graphitron-java-codegen/src/test/resources/queries/edit/default/expected/MutationDBQueries.java
+++ graphitron-codegen-parent/graphitron-java-codegen/src/test/resources/queries/edit/default/expected/MutationDBQueries.java
@@ -5,11 +5,12 @@ import static no.sikt.graphitron.jooq.generated.testdata.pg_catalog.Tables.*;
 
 import java.util.Arrays;
 import no.sikt.graphitron.jooq.generated.testdata.public_.tables.records.CustomerRecord;
+import no.sikt.graphql.NodeIdStrategy;
 import org.jooq.DSLContext;
 import org.jooq.impl.DSL;
 
 public class MutationDBQueries {
-    public static int mutationForMutation(DSLContext ctx, CustomerRecord inRecord) {
+    public static int mutationForMutation(DSLContext ctx, NodeIdStrategy nodeIdStrategy, CustomerRecord inRecord) {
         return ctx.transactionResult(config -> Arrays.stream(DSL.using(config).batchUpdate(inRecord).execute()).sum());
     }
 }
--- graphitron-codegen-parent/graphitron-java-codegen/src/test/resources/queries/edit/twoRecords/expected/MutationDBQueries.java
+++ graphitron-codegen-parent/graphitron-java-codegen/src/test/resources/queries/edit/twoRecords/expected/MutationDBQueries.java
@@ -6,11 +6,12 @@ import static no.sikt.graphitron.jooq.generated.testdata.pg_catalog.Tables.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import no.sikt.graphitron.jooq.generated.testdata.public_.tables.records.CustomerRecord;
+import no.sikt.graphql.NodeIdStrategy;
 import org.jooq.DSLContext;
 import org.jooq.impl.DSL;
 
 public class MutationDBQueries {
-    public static int mutationForMutation(DSLContext ctx, CustomerRecord in1Record, CustomerRecord in2Record) {
+    public static int mutationForMutation(DSLContext ctx, NodeIdStrategy nodeIdStrategy, CustomerRecord in1Record, CustomerRecord in2Record) {
         var recordList = new ArrayList();
         recordList.add(in1Record);
         recordList.add(in2Record);
