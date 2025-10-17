f797c00c0c0f5463c95701d1ec1b372a3e0d589b
Alf Lervag <alf.lervag@sikt.no>
2025-10-17 11:30:33 +0200

    Lagt til litt dokumentasjon

--- graphitron-common/src/main/java/no/sikt/graphql/GraphitronContext.java
+++ graphitron-common/src/main/java/no/sikt/graphql/GraphitronContext.java
@@ -4,9 +4,27 @@ import graphql.schema.DataFetchingEnvironment;
 import org.jooq.DSLContext;
 
 public interface GraphitronContext {
+    /***
+     * Used by Graphitron so it can access the database.
+     *
+     * @param env An object containing information about what is being fetched etc. See https://www.graphql-java.com/documentation/data-fetching/#the-interesting-parts-of-the-datafetchingenvironment for more information.
+     * @return The jOOQ DSLContext that Graphitron should use.
+     */
     DSLContext getDslContext(DataFetchingEnvironment env);
 
+    /***
+     * TODO: This will be used by Graphitron to get the value for a contextArgument. It's currently not being used.
+     * @param env An object containing information about what is being fetched etc. See https://www.graphql-java.com/documentation/data-fetching/#the-interesting-parts-of-the-datafetchingenvironment for more information.
+     * @param name The name of the contextArgument we're looking for
+     * @return The value of the contextArgument
+     * @param <T> The type of the contextArgument
+     */
     <T> T getContextArgument(DataFetchingEnvironment env, String name);
 
+    /***
+     * Used by Graphitron when it needs to use a DataLoader to carry out the operation.
+     * @param env An object containing information about what is being fetched etc. See https://www.graphql-java.com/documentation/data-fetching/#the-interesting-parts-of-the-datafetchingenvironment for more information.
+     * @return The name of the DataLoader that should be used
+     */
     String getDataLoaderName(DataFetchingEnvironment env);
 }
