a513497bc2bb7085e0b10def2267c6b876714a68
Alf Lervag <alf.lervag@sikt.no>
2025-10-23 19:19:26 +0200

    Lagt til test som viser at transform også fungerer uten Node

--- graphitron-schema-transform/src/test/java/no/fellesstudentsystem/schema_transformer/transform/AddKeyDirectivesTest.java
+++ graphitron-schema-transform/src/test/java/no/fellesstudentsystem/schema_transformer/transform/AddKeyDirectivesTest.java
@@ -10,6 +10,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AddKeyDirectivesTest {
     @Test
+    public void testWithoutNode() {
+        var registry = new SchemaParser().parse("""
+                directive @key(fields: federation__FieldSet!, resolvable: Boolean = true) repeatable on INTERFACE | OBJECT
+                scalar federation__FieldSet
+
+                type Query {
+                    someType(
+                        param: String!
+                    ): [SomeType]
+                }
+
+                type SomeType {
+                    id: ID!
+                    field: String!
+                }
+                """);
+        AddKeyDirectives.transform(registry);
+
+        var actual = new SchemaPrinter(SchemaPrinter.Options.defaultOptions()
+                .includeDirectiveDefinitions(false)
+                .setComparators(GraphqlTypeComparatorRegistry.AS_IS_REGISTRY))
+                .print(UnExecutableSchemaGenerator.makeUnExecutableSchema(registry));
+
+        assertEquals("""
+                type Query {
+                  someType(param: String!): [SomeType]
+                }
+
+                type SomeType {
+                  id: ID!
+                  field: String!
+                }
+
+                scalar federation__FieldSet
+                """, actual);
+    }
+
+    @Test
     public void testTransform() {
         var registry = new SchemaParser().parse("""
                 directive @key(fields: federation__FieldSet!, resolvable: Boolean = true) repeatable on INTERFACE | OBJECT
