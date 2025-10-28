61f390863906a69824faaa2f21c17624038d63ed
Alf Lervag <alf.lervag@sikt.no>
2025-10-28 12:33:47 +0100

    Unngå duplikate @key(fields: "id")-direktiver

--- graphitron-schema-transform/src/main/java/no/fellesstudentsystem/schema_transformer/transform/AddKeyDirectives.java
+++ graphitron-schema-transform/src/main/java/no/fellesstudentsystem/schema_transformer/transform/AddKeyDirectives.java
@@ -9,9 +9,26 @@ public class AddKeyDirectives {
     public static void transform(TypeDefinitionRegistry typeDefinitionRegistry) {
         var implementors = typeDefinitionRegistry.getImplementationsOf(new InterfaceTypeDefinition("Node"));
         for (var oldType : implementors) {
+            var alreadyHasNodeKeyDirective = oldType.getDirectives("key").stream()
+                    .anyMatch(AddKeyDirectives::isNodeKeyDirective);
+
+            if (alreadyHasNodeKeyDirective) {
+                continue;
+            }
+
             var newType = oldType.transform(it -> it.directive(new Directive("key", List.of(new Argument("fields", new StringValue("id"))))));
             typeDefinitionRegistry.remove(oldType);
             typeDefinitionRegistry.add(newType);
         }
+    }
+
+    private static boolean isNodeKeyDirective(Directive it) {
+        var arg = it.getArgument("fields");
+        var val = arg.getValue();
+        if (val instanceof StringValue s) {
+            return "id".equals(s.getValue());
+        }
+
+        return false;
     }
 }
--- graphitron-schema-transform/src/test/java/no/fellesstudentsystem/schema_transformer/transform/AddKeyDirectivesTest.java
+++ graphitron-schema-transform/src/test/java/no/fellesstudentsystem/schema_transformer/transform/AddKeyDirectivesTest.java
@@ -90,4 +90,48 @@ class AddKeyDirectivesTest {
                 scalar federation__FieldSet
                 """, actual);
     }
+
+    @Test
+    public void testDuplicateKeyTransform() {
+        var registry = new SchemaParser().parse("""
+                directive @key(fields: federation__FieldSet!, resolvable: Boolean = true) repeatable on INTERFACE | OBJECT
+                scalar federation__FieldSet
+
+                interface Node { id: ID! }
+
+                type Query {
+                    someType(
+                        param: String!
+                    ): [SomeType]
+                }
+
+                type SomeType implements Node @key(fields: "id") {
+                    id: ID!
+                    field: String!
+                }
+                """);
+        AddKeyDirectives.transform(registry);
+
+        var actual = new SchemaPrinter(SchemaPrinter.Options.defaultOptions()
+                .includeDirectiveDefinitions(false)
+                .setComparators(GraphqlTypeComparatorRegistry.AS_IS_REGISTRY))
+                .print(UnExecutableSchemaGenerator.makeUnExecutableSchema(registry));
+
+        assertEquals("""
+                interface Node {
+                  id: ID!
+                }
+
+                type Query {
+                  someType(param: String!): [SomeType]
+                }
+
+                type SomeType implements Node @key(fields : "id", resolvable : true) {
+                  id: ID!
+                  field: String!
+                }
+
+                scalar federation__FieldSet
+                """, actual);
+    }
 }
