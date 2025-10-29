569b8e667253bc7fd21cc1ecf66d88d8beaa0373
Alf Lervåg <alf.lervag@sikt.no>
2025-10-29 12:53:09 +0100

    Switching to using the default parser options for SDL handling (#224)
    We've been using the default options for parsing queries.

--- graphitron-common/src/main/java/no/sikt/graphql/schema/SchemaReadingHelper.java
+++ graphitron-common/src/main/java/no/sikt/graphql/schema/SchemaReadingHelper.java
@@ -20,36 +20,21 @@ import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static graphql.parser.ParserOptions.MAX_QUERY_CHARACTERS;
-import static graphql.parser.ParserOptions.MAX_QUERY_TOKENS;
 
 /**
  * Class for reading schema files from disk and classpath.
  */
 public class SchemaReadingHelper {
-    /**
-     * Default is 15000 for the GraphQL parser. With a new directive on every field it goes over this limit.
-     * If the errors about preventing DoS attacks show up again, increase the values in this file.
-     * DoS is not relevant here since this is only used for reading local schema files.
-     */
-    private final static int MAX_TOKENS = MAX_QUERY_TOKENS * 12;
-    /**
-     * Default is 1 MB (1024 * 1024) for the GraphQL parser.
-     */
-    private final static int MAX_CHARACTERS = MAX_QUERY_CHARACTERS * 3;
 
     public static Document readSchemas(Collection<String> sources) {
         MultiSourceReader.Builder builder = MultiSourceReader.newMultiSourceReader();
         sources.forEach(path -> builder.string(readContent(path) + System.lineSeparator(), path));
 
-        var parseOptions = ParserOptions
-                .getDefaultParserOptions()
-                .transform(build -> build.maxTokens(MAX_TOKENS).maxCharacters(MAX_CHARACTERS));
         var multiSourceReader = builder.trackData(true).build();
         return new Parser()
                 .parseDocument(
                         ParserEnvironment.newParserEnvironment()
-                                .parserOptions(parseOptions)
+                                .parserOptions(ParserOptions.getDefaultSdlParserOptions())
                                 .document(multiSourceReader)
                                 .build());
     }
