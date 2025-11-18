1d976fddee77f6aa469fd87657b06d44e46901b4
Filip Folkesson <filip.folkesson@sikt.no>
2025-11-18 14:51:23 +0100

    Fixed feedback
    Added validation functionality to check that each field and unpacked key values, that map to the same jOOQ column are asserted to have the same value.

--- graphitron-codegen-parent/graphitron-java-codegen/src/test/java/no/sikt/graphitron/datafetchers/standard/edit/NodeIdResolverTest.java
+++ graphitron-codegen-parent/graphitron-java-codegen/src/test/java/no/sikt/graphitron/datafetchers/standard/edit/NodeIdResolverTest.java
@@ -5,15 +5,13 @@ import no.sikt.graphitron.configuration.GeneratorConfig;
 import no.sikt.graphitron.generators.abstractions.ClassGenerator;
 import no.sikt.graphitron.generators.datafetchers.operations.OperationClassGenerator;
 import no.sikt.graphql.schema.ProcessedSchema;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 import java.util.Set;
 
 import static no.sikt.graphitron.common.configuration.SchemaComponent.CUSTOMER_INPUT_TABLE;
+import static no.sikt.graphitron.common.configuration.SchemaComponent.NODE;
 
 @DisplayName("Mutation resolvers - Resolvers with nodeId for mutations")
 public class NodeIdResolverTest extends GeneratorTest {
@@ -45,6 +43,72 @@ public class NodeIdResolverTest extends GeneratorTest {
                 ".mutationForMutation(_iv_transform.getCtx(), _iv_nodeIdStrategy, _mi_inRecord)",
                 ".mutationForMutation(_iv_ctx, _iv_nodeIdStrategy, _mi_inRecord, _iv_selectionSet)"
         );
+    }
+
+    @Test
+    @DisplayName("Assert overlapping fields")
+    void assertInputColumns() {
+        assertGeneratedContentContains(
+                "assertInputColumns", Set.of(NODE),
+                        """
+                        FilmActorInput in = ResolverHelpers.transformDTO(_iv_env.getArgument("in"), FilmActorInput.class);
+                        var _iv_transform = new RecordTransformer(_iv_env);
+                        var inRecord = _iv_transform.filmActorInputToJOOQRecord(in, _iv_nodeIdStrategy, "in");
+                        var _unpacked_id = in.getId() != null ? _iv_nodeIdStrategy.unpackIdValues("FilmActor", in.getId(), FilmActor.FILM_ACTOR.getPrimaryKey().getFieldsArray()) : null;
+                        if (!Objects.equals(_unpacked_id != null ? _iv_nodeIdStrategy.getFieldValue(FilmActor.FILM_ACTOR.ACTOR_ID, _unpacked_id[0]) : null, in.getActor())) {
+                            throw new IllegalArgumentException("Field id and field actor differs in value but writes to the same column.");
+                        }
+                        """
+        );
+    }
+
+    @Test
+    @DisplayName("Assert overlapping fields with listed input record")
+    void assertListedInputColumns() {
+        assertGeneratedContentContains(
+                "assertListedInputColumns", Set.of(NODE),
+                """
+                        List<FilmActorInput> in = ResolverHelpers.transformDTOList(_iv_env.getArgument("in"), FilmActorInput.class);
+                        var _iv_transform = new RecordTransformer(_iv_env);
+                        var inRecordList = _iv_transform.filmActorInputToJOOQRecord(in, _iv_nodeIdStrategy, "in");
+                        for (var itIn : in) {
+                            var _unpacked_id = itIn.getId() != null ? _iv_nodeIdStrategy.unpackIdValues("FilmActor", itIn.getId(), FilmActor.FILM_ACTOR.getPrimaryKey().getFieldsArray()) : null;
+                            if (!Objects.equals(_unpacked_id != null ? _iv_nodeIdStrategy.getFieldValue(FilmActor.FILM_ACTOR.ACTOR_ID, _unpacked_id[0]) : null, itIn.getActor())) {
+                                throw new IllegalArgumentException("Field id and field actor differs in value but writes to the same column.");
+                            }
+                        }
+                        """
+                );
+    }
+
+    @Test
+    @DisplayName("Assert overlapping NodeIds")
+    void assertOverlappingNodeIds() {
+        assertGeneratedContentContains("assertOverlappingNodeIds", Set.of(NODE),
+                "if (!Objects.equals(_unpacked_id != null ? _iv_nodeIdStrategy.getFieldValue(FilmActor.FILM_ACTOR.ACTOR_ID, _unpacked_id[0]) : null, _unpacked_secondId != null ? _iv_nodeIdStrategy.getFieldValue(FilmActor.FILM_ACTOR.ACTOR_ID, _unpacked_secondId[0]) : null)) {",
+                "throw new IllegalArgumentException(\"Field id and field secondId differs in value but writes to the same column.\");",
+                "!Objects.equals(_unpacked_id != null ? _iv_nodeIdStrategy.getFieldValue(FilmActor.FILM_ACTOR.FILM_ID, _unpacked_id[1]) : null, _unpacked_secondId != null ? _iv_nodeIdStrategy.getFieldValue(FilmActor.FILM_ACTOR.FILM_ID, _unpacked_secondId[1]) : null)) {"
+        );
+    }
+
+    @Test
+    @DisplayName("Assert many ovelapping fields")
+    void assertMultipleOverlappingFields() {
+        assertGeneratedContentContains("assertManyOverlappingFields", Set.of(NODE),
+                """
+                            if (!Objects.equals(_unpacked_id != null ? _iv_nodeIdStrategy.getFieldValue(FilmActor.FILM_ACTOR.ACTOR_ID, _unpacked_id[0]) : null, _unpacked_secondId != null ? _iv_nodeIdStrategy.getFieldValue(FilmActor.FILM_ACTOR.ACTOR_ID, _unpacked_secondId[0]) : null)) {
+                                throw new IllegalArgumentException("Field id and field secondId differs in value but writes to the same column.");
+                            }
+                            if (!Objects.equals(_unpacked_id != null ? _iv_nodeIdStrategy.getFieldValue(FilmActor.FILM_ACTOR.ACTOR_ID, _unpacked_id[0]) : null, itIn.getActor())) {
+                                throw new IllegalArgumentException("Field id and field actor differs in value but writes to the same column.");
+                            }
+                            if (!Objects.equals(_unpacked_secondId != null ? _iv_nodeIdStrategy.getFieldValue(FilmActor.FILM_ACTOR.ACTOR_ID, _unpacked_secondId[0]) : null, itIn.getActor())) {
+                                throw new IllegalArgumentException("Field secondId and field actor differs in value but writes to the same column.");
+                            }
+                            if (!Objects.equals(_unpacked_id != null ? _iv_nodeIdStrategy.getFieldValue(FilmActor.FILM_ACTOR.FILM_ID, _unpacked_id[1]) : null, _unpacked_secondId != null ? _iv_nodeIdStrategy.getFieldValue(FilmActor.FILM_ACTOR.FILM_ID, _unpacked_secondId[1]) : null)) {
+                                throw new IllegalArgumentException("Field id and field secondId differs in value but writes to the same column.");
+                            }
+                        """);
     }
 
     @Test
