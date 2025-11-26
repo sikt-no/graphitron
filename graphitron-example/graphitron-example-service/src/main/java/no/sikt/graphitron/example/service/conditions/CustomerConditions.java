0e45a16f97721ff99488aabf8f4f462414f265ff
Andrea Nakstad <57245846+andreahn@users.noreply.github.com>
2025-11-26 13:18:53 +0100

    Add integration test on root field with query condition

--- graphitron-example/graphitron-example-service/src/main/java/no/sikt/graphitron/example/service/conditions/CustomerConditions.java
+++ graphitron-example/graphitron-example-service/src/main/java/no/sikt/graphitron/example/service/conditions/CustomerConditions.java
@@ -2,9 +2,15 @@ package no.sikt.graphitron.example.service.conditions;
 
 import no.sikt.graphitron.example.generated.jooq.tables.Customer;
 import org.jooq.Condition;
+import org.jooq.impl.DSL;
 
 public class CustomerConditions {
     public static Condition activeCustomers(Customer customer) {
         return customer.ACTIVE.eq(1);
+    }
+
+    public static Condition inactiveCustomers(Customer customer, String lastNameStartingWith) {
+        return customer.ACTIVE.eq(0)
+                .and(lastNameStartingWith != null ? customer.LAST_NAME.startsWith(lastNameStartingWith) : DSL.noCondition());
     }
 }
