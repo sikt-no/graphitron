81210e7fc9ce45142810bcf07bcafeb462c3b741
Andrea Nakstad <57245846+andreahn@users.noreply.github.com>
2026-02-26 14:48:32 +0100

    GG-392 generate _entities as lookup query

--- graphitron-codegen-parent/graphitron-java-codegen/src/test/java/no/sikt/graphitron/queries/fetch/EntityTest.java
+++ graphitron-codegen-parent/graphitron-java-codegen/src/test/java/no/sikt/graphitron/queries/fetch/EntityTest.java
@@ -3,7 +3,7 @@ package no.sikt.graphitron.queries.fetch;
 import no.sikt.graphitron.common.GeneratorTest;
 import no.sikt.graphitron.common.configuration.SchemaComponent;
 import no.sikt.graphitron.generators.abstractions.ClassGenerator;
-import no.sikt.graphitron.generators.db.DBClassGenerator;
+import no.sikt.graphitron.reducedgenerators.EntityOnlyFetchDBClassGenerator;
 import no.sikt.graphql.schema.ProcessedSchema;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,25 +28,37 @@ public class EntityTest extends GeneratorTest {
 
     @Override
     protected List<ClassGenerator> makeGenerators(ProcessedSchema schema) {
-        return List.of(new DBClassGenerator(schema));
+        return List.of(new EntityOnlyFetchDBClassGenerator(schema));
     }
 
     @Test
-    @DisplayName("One entity exists")
+    @DisplayName("One entity exists with ID key")
     void defaultCase() {
         assertGeneratedContentContains(
                 "default",
-                "ctx, List<Map<String, Object>> _mi_representations,",
-                "customerSortFieldsFor_entities(_mi_representations)",
-                "For_entities(List<Map<String, Object>> _mi_representations",
+                "ctx, Set<Map<String, Object>> _mi_representations,",
                 """
                 var _mi_representations_filtered = _mi_representations
                                 .stream()
                                 .filter(Objects::nonNull)
                                 .filter(_iv_it -> "Customer".equals(_iv_it.get("__typename")))
                                 .toList();
-                """,
-                ".where(_a_customer.hasIds(_mi_representations_filtered.stream().map(_iv_it -> (String) _iv_it.get(\"id\")).toList())"
+                var _a_customer = CUSTOMER.as("customer_2168032777");
+                return _iv_ctx
+                        .select(
+                                DSL.row(_a_customer.getId()),
+                                customerFor_Entity_customer()
+                        )
+                        .from(_a_customer)
+                        .where(_a_customer.hasIds(_mi_representations_filtered.stream().map(_iv_it -> (String) _iv_it.get("id")).toList()))
+                        .fetchMap(_iv_it -> {
+                                    var _iv_rep = new HashMap<String, Object>();
+                                    _iv_rep.put("__typename", "Customer");
+                                    _iv_rep.put("id", _iv_it.value1().value1());
+                                    return _iv_rep;
+                                },
+                                _iv_it -> (_Entity) _iv_it.value2());
+                """
         );
     }
 
@@ -57,13 +69,18 @@ public class EntityTest extends GeneratorTest {
     }
 
     @Test
-    @DisplayName("Entity with two keys")
+    @DisplayName("Entity with two keys, one ID and one String")
     void twoKeys() {
         assertGeneratedContentContains(
                 "twoKeys",
                 "row(_a_customer.getId(),_a_customer.FIRST_NAME)",
                 "where(_a_customer.hasIds(_mi_representations_filtered.stream().map(_iv_it -> (String) _iv_it.get(\"id\")).toList()))" +
-                        ".or(_a_customer.FIRST_NAME.in(_mi_representations_filtered.stream().map(_iv_it -> (String) _iv_it.get(\"first\")).toList()))"
+                        ".or(_a_customer.FIRST_NAME.in(_mi_representations_filtered.stream().map(_iv_it -> _iv_it.get(\"first\")).toList()))"
+                ,
+                """
+                _iv_rep.put("id", _iv_it.value1().value1());
+                _iv_rep.put("first", _iv_it.value1().value2());
+                """
         );
     }
 
@@ -76,9 +93,14 @@ public class EntityTest extends GeneratorTest {
                 .where(
                         DSL.and(
                                 _a_customer.hasIds(_mi_representations_filtered.stream().map(_iv_it -> (String) _iv_it.get("id")).toList()),
-                                _a_customer.FIRST_NAME.in(_mi_representations_filtered.stream().map(_iv_it -> (String) _iv_it.get("first")).toList())
+                                _a_customer.FIRST_NAME.in(_mi_representations_filtered.stream().map(_iv_it -> _iv_it.get("first")).toList())
                         )
                 )
+                """
+                ,
+                """
+                _iv_rep.put("id", _iv_it.value1().value1());
+                _iv_rep.put("first", _iv_it.value1().value2());
                 """
         );
     }
@@ -86,6 +108,8 @@ public class EntityTest extends GeneratorTest {
     @Test
     @DisplayName("Entity query with integer key")
     void nonStringKey() {
-        assertGeneratedContentContains("nonStringKey", "P_FILM_COUNT.in(_mi_representations_filtered.stream().map(_iv_it -> (Integer) _iv_it.get(\"p_film_count\"))");
+        assertGeneratedContentContains("nonStringKey",
+                "P_FILM_COUNT.in(_mi_representations_filtered.stream().map(_iv_it -> _iv_it.get(\"p_film_count\"))"
+        );
     }
 }
