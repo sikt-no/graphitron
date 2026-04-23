2d900bd28a95d562440c4ff38aa9c63f521facbb
Andrea Nakstad <57245846+andreahn@users.noreply.github.com>
2026-04-23 10:08:16 +0200

    Turn warning into error message

--- graphitron-codegen-parent/graphitron-java-codegen/src/main/java/no/sikt/graphitron/validation/messages/InputDirectiveError.java
+++ graphitron-codegen-parent/graphitron-java-codegen/src/main/java/no/sikt/graphitron/validation/messages/InputDirectiveError.java
@@ -1,11 +1,11 @@
 package no.sikt.graphitron.validation.messages;
 
-import no.sikt.graphitron.validation.messages.interfaces.WarningMessage;
+import no.sikt.graphitron.validation.messages.interfaces.ErrorMessage;
 
 import static no.sikt.graphql.directives.GenerationDirective.RECORD;
 import static no.sikt.graphql.directives.GenerationDirective.TABLE;
 
-public enum InputDirectiveWarning implements WarningMessage {
+public enum InputDirectiveError implements ErrorMessage {
     RECORD_TYPE_CONFLICT(String.format("Input types can only be mapped to one record category (either a jOOQ record " +
                     "via @%1$s or a Java record via @%2$s), but input type '%%s' has both directives. " +
                     "Remove @%1$s to preserve the existing behaviour.",
@@ -13,7 +13,7 @@ public enum InputDirectiveWarning implements WarningMessage {
 
     private final String msg;
 
-    InputDirectiveWarning(String msg) {
+    InputDirectiveError(String msg) {
         this.msg = msg;
     }
 
